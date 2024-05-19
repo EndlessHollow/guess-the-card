e16b3860cf7a418f5fcab4c61f13e5f2abacf03a
Marek Klier <marekklier@pop-os.home>
2024-05-19 22:16:14 +0200

    feat: add answer page

---
+++ src/ui/components/answer-result/answer-result.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Flex from '@ui/layout/flex'
+import Heading from '@ui/typography/heading'
+import Text from '@ui/typography/text'
+
+import type { TAnswer } from '@/types';
+
+type Props = {
+    answer: TAnswer | undefined
+}
+
+defineProps<Props>()
+
+</script>
+
+<template>
+    <Flex direction="column" justify="center" align="center" height="h-screen" gap="4">
+        <Heading>{{ answer }}</Heading>
+        <Text>{{ answer === 'correct' ? 'You gained 5 seconds' : 'You lost 10 seconds' }}</Text>
+    </Flex>
+</template>
